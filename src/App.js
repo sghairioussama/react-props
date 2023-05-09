ab19338ff9fe3dd4dfed264e896356bf8fa7c41a
sghairi oussama <sghairioussama@gmail.com>
2023-05-09 18:50:05 +0100

    workshope props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Profile from './Component/Profile';
 
 function App() {
+  // alert on button using handleName
+  function handleName() {
+    alert("For more information check linkedin or facebook account on Sghairi Oussama")
+  }
+  
   return (
+        /* calling prop-children and giving them properties */
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Profile name="sghairi oussama" age="34" bio="Open to learn new things" profession="Fullstack js Developer" data={handleName} >
+      <img src="https://st2.depositphotos.com/1104517/11965/v/950/depositphotos_119659092-stock-illustration-male-avatar-profile-picture-vector.jpg" alt="Gojo" />
+      </Profile>
     </div>
   );
 }
